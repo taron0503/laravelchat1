c982c7b381e0a6335c7c6132a42ddb075aa0f925
taron <taron0503@gmail.com>
2019-02-06 15:59:49 +0100

    frontend issues finish

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>header</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mycommon.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
--- resources/views/myhome.blade.php
+++ resources/views/myhome.blade.php
@@ -2,5 +2,32 @@
 
 @section("content")
     <h3>MyHome</h3>
+    <div class="container chat_window">
+        <div class="row">
+            <div class="col-9 card chat_table">
+                <div class="card-body">
+                    @for ($i = 0; $i < 10; $i++)
+                        <div class="d-flex justify-content-end p-1">
+                            <div class="card bg-dark text-white">
+                                <div class="card-body">Hello World!!!!!!!!</div>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+                <div class = "message_part card-footer">
+                    <form action="">
+                        @csrf
+                        <div class="input-group">
+                            <textarea name="" class="form-control" placeholder="Type your message..."></textarea>
+                            <div class = "input-group-append">
+                               <button type = "submit" class = "btn btn-primary">Send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-3 users_table"></div>
+        </div>
+    </div>
 
 @endsection
